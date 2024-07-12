4bc615c698e606a3cc50257d58446a2c4e078f6e
Andrey <volgin_andrey27@mail.ru>
2024-07-12 10:01:03 +0500

    Update StreamCollectorsExample.java
    добавлено второе задание

--- src/StreamCollectorsExample.java
+++ src/StreamCollectorsExample.java
@@ -1,5 +1,7 @@
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentMap;
 import java.util.stream.Collectors;
 
 class Order {
@@ -56,5 +58,36 @@ public class StreamCollectorsExample {
         Double sumOfTop3Expensive = top3Expensive.stream()
                 .collect(Collectors.summingDouble(Order::getCost));
         System.out.println(sumOfTop3Expensive);
+    }
+}
+class Student {
+    private String name;
+    private Map<String, Integer> grades;
+
+    public Student(String name, Map<String, Integer> grades) {
+        this.name = name;
+        this.grades = grades;
+    }
+
+    public Map<String, Integer> getGrades() {
+        return grades;
+    }
+}
+
+class ParallelStreamCollectMapAdvancedExample {
+    public static void main(String[] args) {
+        List<Student> students = Arrays.asList(
+                new Student("Student1", Map.of("Math", 90, "Physics", 85)),
+                new Student("Student2", Map.of("Math", 95, "Physics", 88)),
+                new Student("Student3", Map.of("Math", 88, "Chemistry", 92)),
+                new Student("Student4", Map.of("Physics", 78, "Chemistry", 85))
+        );
+        ConcurrentMap<String, Double> averageGradePerPredmet = students.parallelStream()
+                .flatMap(student -> student.getGrades().entrySet().stream())
+                .collect(Collectors.groupingByConcurrent(Map.Entry::getKey,
+                        Collectors.averagingDouble(Map.Entry::getValue)));
+
+        averageGradePerPredmet.forEach((predmet, averageGrade)
+                -> System.out.println(predmet + " " + averageGrade));
     }
 }
